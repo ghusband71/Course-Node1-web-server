84ea94944ea3ceca762c503b2063b6e2bc8a187e
Grayden Husband <graydenh@gmail.com>
2017-01-07 09:45:23 +1100

    initialize repo

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log("private route hit!");
+		next();
+	},
+	logger: function(req, res, next){
+		console.log("Request: (" + new Date().toString() + ") " + req.method + " " + req.originalUrl);
+		next();
+	}
+}
+
+// this is application leve middleware
+// applies to all calls, all routes
+//app.use(middleware.requireAuthentication);
+
+app.get("/hello", function(req, res){
+	res.send("Hello");
+});
+
+// now add route-level middleware
+// add as second argument
+app.get("/about", middleware.requireAuthentication, function(req, res){
+	res.send("About Us");
+});
+
+app.get("/logger", middleware.logger, function(req, res){
+	res.send("Logger Test");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(PORT, function(){
+	console.log("server started on port " + PORT);
+});
